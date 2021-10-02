c687cdf69fdbd31a049f9f5b5a44c7b635cdccb8
IvanGTR <ivan_tr43@outlook.com>
2021-10-01 20:49:35 -0500

    Inicializa y configura loas fragemnts

--- app/src/main/java/com/ivangtr/snapshots/MainActivity.kt
+++ app/src/main/java/com/ivangtr/snapshots/MainActivity.kt
@@ -2,11 +2,48 @@ package com.ivangtr.snapshots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.ivangtr.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var mActivityFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setUpBottomNav()
+
+    }
+
+    private fun setUpBottomNav(){
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActivityFragment = homeFragment
+
+        mFragmentManager
+            .beginTransaction()
+            .add(R.id.hostFragment, profileFragment, profileFragment::class.java.name)
+            .hide(profileFragment)
+            .commit()
+        mFragmentManager
+            .beginTransaction()
+            .add(R.id.hostFragment, addFragment, addFragment::class.java.name)
+            .hide(addFragment)
+            .commit()
+        mFragmentManager
+            .beginTransaction()
+            .add(R.id.hostFragment, homeFragment, homeFragment::class.java.name)
+            .commit()
     }
 }
 
